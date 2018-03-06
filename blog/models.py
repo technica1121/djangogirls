d0f7b02f740c435557614f6209be2d8356c899da
technica1121 <technica1121@docomo.ne.jp>
2018-03-06 12:11:02 +0900

    imagefield pillow

--- blog/models.py
+++ blog/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    image = models.ImageField() #
     text = models.TextField()
     created_date = models.DateTimeField(
             default=timezone.now)
