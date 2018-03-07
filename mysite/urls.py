a147cea75abc50bcd0be45d59ed0cb70a0dfde20
technica1121 <technica1121@docomo.ne.jp>
2018-03-08 08:48:43 +0900

    ittan

--- mysite/urls.py
+++ mysite/urls.py
@@ -26,5 +26,4 @@ urlpatterns = [
     url(r'^accounts/login/$', views.login, name='login'),
     url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
-    static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT),
     ]
